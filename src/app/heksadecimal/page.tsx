9c759f49ecdcd79f0e74803d5d5ac675ccff1b8c
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-08-10 18:16:27 +0700

    comming soon page

---
+++ src/app/heksadecimal/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Heksadecimal = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <h3 className="text-5xl font-bold text-center md:text-7xl">
+        COMING SOON...
+      </h3>
+    </div>
+  );
+};
+
+export default Heksadecimal;
