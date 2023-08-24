0b93f42535a33a31af766db1aa00a995e0f88282
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-08-24 22:26:57 +0700

    update theme color navbar/hamburger menu

--- src/components/atoms/Hamburger.tsx
+++ src/components/atoms/Hamburger.tsx
@@ -5,9 +5,9 @@ interface Props {
 const Hamburger = ({ handleToggle }: Props) => {
   return (
     <div onClick={handleToggle} className="flex flex-col gap-1">
-      <div className="rounded-lg w-[23px] h-[4px] bg-white" />
-      <div className="rounded-lg w-[23px] h-[4px] bg-white" />
-      <div className="rounded-lg w-[23px] h-[4px] bg-white" />
+      <div className="rounded-lg w-[23px] h-[4px] bg-retroWhite" />
+      <div className="rounded-lg w-[23px] h-[4px] bg-retroWhite" />
+      <div className="rounded-lg w-[23px] h-[4px] bg-retroWhite" />
     </div>
   );
 };
