1759b64ff244ac4c7b3a3f6b3bb1db7399bcf3f6
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-08-25 08:03:41 +0700

    update rounded link style

--- src/components/molecules/Sidebar.tsx
+++ src/components/molecules/Sidebar.tsx
@@ -8,14 +8,14 @@ import { RouteLinks } from "../tamplates/RouteLinks";
 const Sidebar = ({ classname }) => {
   const pathname = usePathname();
   return (
-    <main
+    <div
       className={clsx(
         classname,
         "hidden md:flex",
         "relative border-r-2 border-retroBrown/10 h-full flex flex-col items-start justify-start p-1 md:p-5 gap-7 text-center transition-all"
       )}
     >
-      <div className="flex flex-col w-full gap-2 text-lg tracking-widest text-center transition-all">
+      <ul className="flex flex-col w-full gap-2 text-lg tracking-widest text-center transition-all">
         {RouteLinks.map((item, index) => {
           return (
             <Link
@@ -25,16 +25,16 @@ const Sidebar = ({ classname }) => {
                 pathname == item.route
                   ? "text-retroWhite bg-retroBrown"
                   : "text-retroWhite bg-retroBrown/60",
-                "w-full text-start rounded-xl px-2 py-1 font-semibold tracking-wide transition-all"
+                "w-full text-start rounded-md px-2 py-1 font-semibold tracking-wide transition-all"
               )}
             >
               {item.title}
             </Link>
           );
         })}
-      </div>
+      </ul>
       <BsShieldFillExclamation className="absolute text-xl transition-all cursor-pointer text-retroBrown bottom-2 left-2 hover:text-2xl" />
-    </main>
+    </div>
   );
 };
 
