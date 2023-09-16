b9b066e56a9cc9c178b6beb6200d5d5e784c9849
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-09-16 21:04:11 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,12 +14,13 @@ export default function Home() {
   }, [pathname, router]);
 
   return (
-    <div className="flex items-center justify-center w-full h-full text-5xl font-bold">
-      LOADING
-      <span className="loading loading-ball loading-xs" />
-      <span className="loading loading-ball loading-sm" />
-      <span className="loading loading-ball loading-md" />
-      <span className="loading loading-ball loading-lg" />
+    <div className="flex items-center justify-center w-full h-full">
+      <h3 className="text-3xl font-bold text-center md:text-6xl text-retroPink">
+        LOADING
+        <span className="loading loading-ball loading-lg" />
+        <span className="loading loading-ball loading-lg" />
+        <span className="loading loading-ball loading-lg" />
+      </h3>
     </div>
   );
 }
